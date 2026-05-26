7fe914c4b1ca9c94fa42fbcf007bf3e385a9c55f
mr-weed <shubhamw881@gmail.com>
2026-05-26 22:15:08 +0530

    feat: main scene setup

---
+++ scripts/main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Keep this script empty or use it for global level setup (spawning, UI, score)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
